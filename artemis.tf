1abef2f3d4d8a8b22978017f2d701c407aff46be
farrukh90 <s>
2022-04-25 01:16:27 +0000

    updated svc

--- artemis.tf
+++ artemis.tf
@@ -28,9 +28,6 @@ ingress:
   - host: "artemis-${var.namespace}.${var.google_domain_name}"
     paths: 
     - path: /
-      backend:
-        serviceName: artemis
-        servicePort: 5000
   tls: 
   - secretName: artemis
     hosts:
@@ -41,35 +38,36 @@ ingress:
 
 
 
-# module "artemis-terraform-helm-9-0-0" {
-#   source               = "./modules/terraform-helm/"
-#   deployment_name      = "artemis-9-0-0"
-#   deployment_namespace = var.namespace
-#   deployment_path      = "charts/artemis/"
-#   values_yaml          = <<EOF
-# image:
-#   repository: "${var.image}"
-#   tag: "9.0.0"
-# service:
-#   type: ClusterIP
-#   port: 5000
+module "artemis-terraform-helm-9-0-0" {
+  source               = "./modules/terraform-helm/"
+  deployment_name      = "artemis-9-0-0"
+  deployment_namespace = var.namespace
+  deployment_path      = "charts/artemis/"
+  values_yaml          = <<EOF
+image:
+  repository: "${var.image}"
+  tag: "9.0.0"
+service:
+  type: ClusterIP
+  port: 5000
 
-# ingress:
-#   enabled: true
-#   annotations:
-#     nginx.ingress.kubernetes.io/proxy-body-size: "64m"
-#     ingress.kubernetes.io/ssl-redirect: "false"
-#     cert-manager.io/cluster-issuer: letsencrypt-prod
-#     acme.cert-manager.io/http01-edit-in-place: "true"
-#     kubernetes.io/ingress.class: nginx
-#   hosts:
-#   - host: "artemis-9.0.0-${var.namespace}.${var.google_domain_name}"
-#     paths: 
-#     - path: /
-#   tls: 
-#   - secretName: artemis-9.0.0
-#     hosts:
-#     - "artemis-9.0.0-${var.namespace}.${var.google_domain_name}"
-#   ingressClassName: nginx
-#   EOF
-# }
+ingress:
+  enabled: true
+  annotations:
+    nginx.ingress.kubernetes.io/proxy-body-size: "64m"
+    ingress.kubernetes.io/ssl-redirect: "false"
+    cert-manager.io/cluster-issuer: letsencrypt-prod
+    acme.cert-manager.io/http01-edit-in-place: "true"
+    kubernetes.io/ingress.class: nginx
+  hosts:
+  - host: "artemis-9.0.0-${var.namespace}.${var.google_domain_name}"
+    paths: 
+    - path: /
+
+  tls: 
+  - secretName: artemis-9.0.0
+    hosts:
+    - "artemis-9.0.0-${var.namespace}.${var.google_domain_name}"
+  ingressClassName: nginx
+  EOF
+}
